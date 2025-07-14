bbfc34fddf06582f4c6060be55899a7f8a3bbb66
Balogun Basheer Olamilekan <babtech2global@gmail.com>
2025-07-15 00:41:44 +0100

    database values hidden in .env

--- backend/settings.py
+++ backend/settings.py
@@ -106,11 +106,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'mwtrofifbRIQXtYQsliXwopsSxhrVLcc',
-        'HOST': 'caboose.proxy.rlwy.net',
-        'PORT': '34493',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
